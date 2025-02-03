215e0c8d832cd4133e537d2adc7806021a0721c0
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2025-02-03 01:10:35 -0800

    fixed project inconsistencies

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -142,7 +142,6 @@ export default function Landing() {
                     <AvatarFallback>:P</AvatarFallback>
                   </Avatar>
                 </div>
-
               </div>
             </section>
 
@@ -153,22 +152,22 @@ export default function Landing() {
               <div className="grid lg:grid-cols-[1fr_1fr] sm:grid-cols-1 items-stretch gap-4">
                 <ProjectCard title={"Planview Copilot"} date={"Jan 2024 - Present"}
                              technologies={["React", "Java Spring Boot", "PostgreSQL", "AWS", "Docker"]}
-                             description={"Full stack web development and messing with LLMs."} githubLink={"https://www.planview.com/ai/"}/>
+                             description={"Full stack web development and messing with LLMs"} githubLink={"https://www.planview.com/ai/"}/>
                 <ProjectCard title={"Ride Rater"} date={"Jul 2024 - Present"}
                              technologies={["React", "Java Spring Boot", "PostgreSQL", "Docker"]}
                              description={"Full stack app that allows real riders to rate rides at various amusement parks"} githubLink={"https://github.com/rzhang57/riderater"}/>
                 <ProjectCard title={"Kumon Homework Grader"} date={"Jun 2024 - Present"}
                              technologies={["Tensorflow", "Keras", "Flask", "React", "PostgreSQL", "OpenCV"]}
                              description={"Homework grading automation using OpenCV and Keras model trained on real student's handwriting"}/>
-                <ProjectCard title={"Portfolio website (this!)"} date={"Sept 2024"}
+                <ProjectCard title={"Portfolio Website"} date={"Sept 2024"}
                              technologies={["Typescript", "Next.js"]}
                              description={"My personal portfolio website - about me and what I'm working on"}
                              githubLink={"https://github.com/rzhang57/rmzhang-website"}/>
-                <ProjectCard title={"Braille/ English Translator"} date={"Sept 2024"}
+                <ProjectCard title={"Braille + English Translator"} date={"Sept 2024"}
                              technologies={["Python"]}
                              description={"Command line application that translates Braille to English and vice versa"}
                              githubLink={"https://github.com/rzhang57/eng-intern-challenge"}/>
-                <ProjectCard title={"Exam preparer"} date={"Jan 2024 - Apr 2024"}
+                <ProjectCard title={"Exam Preparer"} date={"Jan 2024 - Apr 2024"}
                              technologies={["Java", "Java Swing", "JUnit"]}
                              description={"Desktop application that helps users prepare for exams by storing practice problems and notes"}
                              githubLink={"https://github.com/rzhang57/exam-preparer"}/>
