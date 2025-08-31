375e80d44b5d47218aca7e5ba351a43c9d0b6bf5
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2025-08-31 16:23:33 -0700

    remove confetti and redirect to about section

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,37 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import Confetti from "react-confetti";
 import Link from "next/link";
 
 export default function Hero() {
-  const [recycleConfetti, setRecycleConfetti] = useState(false);
-  const [showConfetti, setShowConfetti] = useState(false);
-
-  function handleNameClick() {
-    setShowConfetti(true);
-    setRecycleConfetti(true);
-    setTimeout(() => setRecycleConfetti(false), 500);
-  }
 
   return (
     <div className="space-y-2 text-center relative">
-      {showConfetti && (
-        <Confetti
-          width={typeof window !== "undefined" ? window.innerWidth - 17 : 0}
-          height={typeof window !== "undefined" ? window.innerHeight + 100 : 0}
-          recycle={recycleConfetti}
-          numberOfPieces={100}
-        />
-      )}
       <h1
-        onClick={handleNameClick}
+        onClick={() => {
+            window.location.href = "#about";
+        }}
         className="group relative font-bold tracking-tighter hover:tracking-normal transition-all text-6xl py-4 duration-1000 hover:cursor-pointer"
       >
         ryan zhang
       </h1>
       <div className="inline text-muted-foreground md:text-3xl text-center self-center tracking-tighter leading-relaxed">
-        software engineer coop at{'  '}
+        previous swe intern at{'  '}
         <Link
           href="https://www.planview.com/"
           target="_blank"
