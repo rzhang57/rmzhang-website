8f04219b35992160ccecf8a7418d88a1acb5a3ce
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2025-11-26 12:47:17 -0800

    make leading relaxed in work experience

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -123,10 +123,11 @@ export default function About() {
                         <h3 className="text-foreground font-medium"><span
                             className="font-bold text-[#6ca1ff] italic">{exp.company} {'>'} </span> {exp.role}</h3>
                         <p className="font-light">{exp.overview}</p>
-                        <ExpandableCard title={<span className="text-sm italic text-muted-foreground/80">more details</span>}>
-                            <ul className="list-disc list-inside mt-2 pl-2 space-y-1 text-muted-foreground text-base">
+                        <ExpandableCard
+                            title={<span className="text-sm italic text-muted-foreground/80">more details</span>}>
+                            <ul className="list-disc list-outside mt-2 pl-6 space-y-1 text-muted-foreground text-base">
                                 {exp.description.map((point, i) => (
-                                    <li key={i}>{point}</li>
+                                    <li key={i} className="leading-relaxed">{point}</li>
                                 ))}
                             </ul>
                         </ExpandableCard>
