7c00d0a4baa3fa443afa4c3a624df8124b287792
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2025-12-16 18:40:10 -0800

    upgrade next

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,7 +21,7 @@ export default function App() {
         <>
             <Head>
                 <title>ryan m zhang</title>
-                <meta name="description" content="about ryan zhang" />
+                <meta name="description" content="ryan zhang" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             {isLoading ? (
